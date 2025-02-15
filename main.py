d44a60bcf9f15374387404c20a5262d28a5366b9
suranimayur <surani.mayur@gmail.com>
2025-02-15 13:33:41 +0530

    Logging Functionality

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.datascience import logger
+
+
+logger.info("Welcome to our custom logging datascience project")
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from src.datascience import logger
+
+
+logger.info("Welcome to our custom logging datascience project")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
